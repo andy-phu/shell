168a5a6466228a964c5357bb8b3b60a2c295108a
Benjamin <bylin1@uci.edu>
2023-10-15 20:39:30 -0700

    Stage 1 Complete

---
+++ hw2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include<unistd.h> 
+
+
+int main() {
+    char input[128];
+    
+    while (1) {
+
+        printf("prompt > ");
+        
+        if (fgets(input, 128, stdin) == NULL) {
+            perror("Failed to read input");
+            exit(1);
+        }
+
+        char *command = strtok(input, " \n");
+        if (command == NULL) {
+            continue;
+        }
+
+        if (strcmp(command, "cd") == 0) {
+            char *arg = strtok(NULL, " \n");
+            if (arg == NULL) {
+                printf("cd: missing directory\n");
+            } else {
+                if (chdir(arg) != 0) {
+                    perror("cd");
+                }
+            }
+        } else if (strcmp(command, "pwd") == 0) {
+            char currentDir[128];
+            if (getcwd(currentDir, sizeof(currentDir)) != NULL) {
+                printf("%s\n", currentDir);
+            } else {
+                perror("pwd");
+            }
+        } else if (strcmp(command, "quit") == 0) {
+            break;
+        } else {
+            printf("Error: Invalid command.\n");
+        }
+    }
+
+    printf("Shell terminated.\n");
+    return 0;
+}
