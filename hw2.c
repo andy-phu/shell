3287c5954dd2eca624eb2449cb224b4fe3f32922
Benjamin Lin <benjilin25@gmail.com>
2023-10-17 16:39:04 -0700

    made updates to stage 2

--- hw2.c
+++ hw2.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include<unistd.h> 
+#include <unistd.h> 
+#include <signal.h>
+#include <sys/wait.h>
 
+pid_t pid; //signal for parent or child
+pid_t foreground_pid = -1;
+
+void signal_handler(int signal){
+    if (foreground_pid != -1){
+        kill(pid,SIGINT);
+    }
+}
 
 int main() {
+    signal(SIGINT, signal_handler);
+    
     char input[128];
     
     while (1) {
@@ -39,8 +51,26 @@ int main() {
             }
         } else if (strcmp(command, "quit") == 0) {
             break;
+
         } else {
-            printf("Error: Invalid command.\n");
+            if (access(command, X_OK) == 0) {
+                pid_t pid = fork();
+                int status;
+
+                if (pid == 0) {
+                    // Child process
+                    char *args[] = {command, NULL};
+                    if (execv(command, args) == -1) {
+                        perror("execv");
+                        exit(1);
+                    }
+                } else {
+                    // Parent process
+                    waitpid(pid, &status, 0);
+                }
+            } else {
+                printf("Error: Invalid command.\n");
+            }
         }
     }
     //test to see if i can make changes
