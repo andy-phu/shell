8feb56c67db8bcc6386ba42148872261536c0c61
Benjamin Lin <benjilin25@gmail.com>
2023-10-19 23:21:46 -0700

    made changes

--- hw2.c
+++ hw2.c
@@ -7,6 +7,7 @@
 
 pid_t pid; //signal for parent or child
 pid_t foreground_pid = -1;
+int continue_index;
 int job_id_counter = 1;
 
 struct Job{
@@ -21,24 +22,33 @@ struct Job jobs[128];
 
 
 void foreground_handler(int signal){
-    if (foreground_pid != -1){
-        kill(pid,SIGINT);
+    //printf("foreground handler %d\n",getpgid(pid));
+    if (foreground_pid == -1 && getpgid(pid) == getpgid(foreground_pid)){
+        printf("trying to kill the background with command\n");
+    }
+    else if (foreground_pid != -1){
+        kill(foreground_pid, SIGINT);
+
     }
 }
 
 void suspend_handler(int signal){
-    if (foreground_pid != -1){
+    if (foreground_pid == -1 && getpgid(pid) == getpgid(foreground_pid)){
+        printf("trying to suspend the background with command\n");
+    }
+    else if (foreground_pid != -1){
         for (int i = 0; i < job_id_counter; i++) {
             if (jobs[i].pid == foreground_pid) {
                 jobs[i].state = 2;
             }
         }
-        printf("child process with PID: %d suspended\n",pid);
+        printf("child process with PID: %d suspended\n",foreground_pid);
         kill(foreground_pid,SIGTSTP);
         foreground_pid = -1;
 
     } 
 }
+
 
 void background_handler(int signal){
     int child_status;
@@ -54,10 +64,21 @@ void background_handler(int signal){
     }
 }
 
+void continue_handler(int signal){
+    int status;
+    // int waitCondition = WUNTRACED | WCONTINUED;
+    // tcsetpgrp(STDIN_FILENO, jobs[continue_index].pid);
+    printf("here");
+    printf("%d", jobs[continue_index].pid);
+    waitpid(jobs[continue_index].pid, &status, WUNTRACED);
+    printf("\ncontinue");
+}
+
 int main() {
     signal(SIGINT, foreground_handler);
     signal(SIGCHLD, background_handler);
     signal(SIGTSTP, suspend_handler);
+    // signal(SIGCONT, continue_handler);
 
     char input[128];
     
@@ -93,11 +114,29 @@ int main() {
             } else {
                 perror("pwd");
             }
+        }else if (strcmp(command, "fg") == 0) {
+            if (arg != NULL && strcmp(arg, "%") == 0){
+                kill(pid, SIGCONT);
+            }else{
+                int temp_pid = atoi(arg);
+                int status;
+                for (int i = 0; i < job_id_counter; i++) {
+                    if (jobs[i].pid == temp_pid) {
+                        jobs[i].state = 0;
+                        continue_index = i;
+                        if (kill(jobs[i].pid, SIGCONT) == 0){
+                            foreground_pid = jobs[continue_index].pid;
+                            waitpid(jobs[continue_index].pid, &status, WUNTRACED);
+                            foreground_pid = -1;
+                        }
+                    }
+                }
+            }
         } else if (strcmp(command, "quit") == 0) {
             break;
         } else if (strcmp(command, "jobs") == 0){
-            for (int i = 0; i < job_id_counter; i++) {
-                if (jobs[i].state != 0) {
+            for (int i = 1; i < job_id_counter; i++) {
+                if (jobs[i].job_id > 0){
                     char *status;
                     if (jobs[i].state == 0 || jobs[i].state == 1){
                         status = "Running";
@@ -112,6 +151,7 @@ int main() {
         } else {
             if (access(command, X_OK) == 0) 
             {
+                int waitCondition = WUNTRACED | WCONTINUED;
                 char cl[128];
                 strcpy(cl, command);
                 pid_t pid = fork();
@@ -119,6 +159,7 @@ int main() {
                 if (arg != NULL && strcmp(arg, "&") == 0){//run the background task 
                     strcat(cl, " ");
                     strcat(cl, arg);
+                    // print("background group id %d",getprp());
                     if (pid == 0) { // Child process
                         char *args[] = {command, NULL};
                         if (execv(command, args) == -1) {
@@ -132,6 +173,7 @@ int main() {
                         jobs[job_id_counter].state = 1;
                         jobs[job_id_counter].command_line = cl;
                         job_id_counter++;
+                        setpgid(pid,pid);
                         printf("Background process running\n");
                     }
 
@@ -145,14 +187,14 @@ int main() {
                             exit(1);
                         }
                     } else {
-                        foreground_pid = getpid();
                         jobs[job_id_counter].pid = pid;
                         jobs[job_id_counter].job_id = job_id_counter;
                         jobs[job_id_counter].state = 0;
                         jobs[job_id_counter].command_line = cl;
                         job_id_counter++;
                         // Parent process
-                        waitpid(pid, &status, 0);
+                        foreground_pid = pid;
+                        waitpid(pid, &status, waitCondition);
                         foreground_pid = -1;
                     }
                 }
